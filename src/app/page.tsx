3082847b028a14f691560f4b782c24da483129ac
caru-ini <s1F102400392@iniad.org>
2024-05-19 23:11:47 +0900

    Format by prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,41 @@
-import Nav from "@/components/nav";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-import { FaEnvelope } from "react-icons/fa6";
-
-import { SiX, SiGithub, SiDiscord } from "react-icons/si";
-import { MdChevronRight } from "react-icons/md";
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaEnvelope } from 'react-icons/fa6';
+import { MdChevronRight } from 'react-icons/md';
+import { SiDiscord, SiGithub, SiX } from 'react-icons/si';
+import Nav from '@/components/nav';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <main>
       <Nav />
-      <div className="flex flex-col items-center space-y-6 justify-center min-h-[calc(100vh-74px)] flex-wrap">
+      <div className='flex flex-col items-center space-y-6 justify-center min-h-[calc(100vh-74px)] flex-wrap'>
         <Image
-          src="/avatar.png"
-          alt="caru"
+          src='/avatar.png'
+          alt='caru'
           width={200}
           height={200}
-          className="rounded-full"
+          className='rounded-full'
         />
-        <h1 className="text-4xl font-bold">Caru</h1>
-        <Link href={"/about"}>
-          <Button variant="secondary">
+        <h1 className='text-4xl font-bold'>Caru</h1>
+        <Link href={'/about'}>
+          <Button variant='secondary'>
             Check My Profile
-            <MdChevronRight className="ml-2" size={24} />
+            <MdChevronRight className='ml-2' size={24} />
           </Button>
         </Link>
-        <div className="flex space-x-4 items-center">
-          <Link href="https://twitter.com/caru_ini_8">
+        <div className='flex space-x-4 items-center'>
+          <Link href='https://twitter.com/caru_ini_8'>
             <SiX size={32} />
           </Link>
-          <Link href="https://github.com/caru-ini">
+          <Link href='https://github.com/caru-ini'>
             <SiGithub size={32} />
           </Link>
-          <Link href="https://discordapp.com/users/1226826654794649690">
+          <Link href='https://discordapp.com/users/1226826654794649690'>
             <SiDiscord size={32} />
           </Link>
-          <Link href="mailto:s1F102400392@iniad.org">
+          <Link href='mailto:s1F102400392@iniad.org'>
             <FaEnvelope size={32} />
           </Link>
         </div>
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,10 @@
-"use client";
-import React from "react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import ToggleTheme from "@/components/toggletheme";
-import { useState, useEffect } from "react";
+'use client';
+
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import ToggleTheme from '@/components/toggletheme';
+import { Button } from '@/components/ui/button';
 
 const Nav: React.FC = () => {
   const [scroll, setScroll] = useState(true);
@@ -15,37 +16,37 @@ const Nav: React.FC = () => {
         setScroll(true);
       }
     };
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   const transition = scroll
-    ? "transform translate-y-0"
-    : "transform -translate-y-full";
+    ? 'transform translate-y-0'
+    : 'transform -translate-y-full';
 
   return (
-    <nav className="sticky z-50 flex justify-center items-center mx-auto top-0">
+    <nav className='sticky z-50 flex justify-center items-center mx-auto top-0'>
       {/* desktop */}
       <div
         className={
-          "flex space-x-1 md:space-x-4 bg-secondary rounded-b-3xl py-4 px-6 border border-border transition-transform duration-300 ease-in-out " +
+          'flex space-x-1 md:space-x-4 bg-secondary rounded-b-3xl py-4 px-6 border border-border transition-transform duration-300 ease-in-out ' +
           transition
         }
       >
-        <Link href="/">
-          <Button variant={"link"} className="text-xl text-foreground">
+        <Link href='/'>
+          <Button variant={'link'} className='text-xl text-foreground'>
             Home
           </Button>
         </Link>
-        <Link href="/about">
-          <Button variant={"link"} className="text-xl text-foreground">
+        <Link href='/about'>
+          <Button variant={'link'} className='text-xl text-foreground'>
             About
           </Button>
         </Link>
-        <Link href="#">
-          <Button variant={"link"} className="text-xl text-muted-foreground">
+        <Link href='#'>
+          <Button variant={'link'} className='text-xl text-muted-foreground'>
             Blog
           </Button>
         </Link>

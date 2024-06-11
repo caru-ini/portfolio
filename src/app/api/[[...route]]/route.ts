a6f2d583231475d970dd26a19463a97622136432
caru-ini <s1F102400392@iniad.org>
2024-06-12 00:50:23 +0900

    feat: Add basic api

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,36 @@
+import { posts } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/d1';
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+
+export const runtime = 'edge';
+
+type Bindings = {
+  DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>().basePath('/api');
+
+app.get('/blog/posts', async (c) => {
+  try {
+    const db = drizzle(c.env.DB);
+    const results = await db.select().from(posts).all();
+    return c.json(results);
+  } catch (e) {
+    return c.json({ error: e }, 500);
+  }
+});
+
+app.get('/blog/posts/:id', async (c) => {
+  try {
+    const db = drizzle(c.env.DB);
+    const id = parseInt(c.req.param().id);
+    const results = await db.select().from(posts).where(eq(posts.id, id)).all();
+    return c.json(results);
+  } catch (e) {
+    return c.json({ error: e }, 500);
+  }
+});
+
+export const GET = handle(app);

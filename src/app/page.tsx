8763ad3d5a7d5a646dff0b6acbc64783a894371d
caru-ini <caru17122@gmail.com>
2024-06-26 23:10:13 +0900

    feat: move qr modal to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { QrCode } from 'lucide-react';
 import { MdChevronRight } from 'react-icons/md';
 import {
   SiDiscord,
@@ -12,36 +10,11 @@ import {
   SiX,
   SiZenn,
 } from 'react-icons/si';
-import ToggleTheme from '@/components/toggletheme';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   return (
     <main className='bg-gradient-to-b from-background to-teal-200/10'>
-      <div className='fixed left-0 top-0 flex gap-2 p-4'>
-        <ToggleTheme variant={'outline'} />
-        <Button
-          variant={'outline'}
-          className='flex items-center space-x-2'
-          onClick={() => setIsModalOpen(true)}
-        >
-          <QrCode size={24} />
-        </Button>
-      </div>
-      {isModalOpen && (
-        <div
-          className='container fixed inset-0 z-50 flex min-h-full min-w-full items-center justify-center bg-background/50'
-          onClick={() => setIsModalOpen(false)}
-        >
-          <div
-            className='select-none gap-3 rounded-lg bg-foreground/30 p-5'
-            onClick={(e) => e.stopPropagation()}
-          >
-            <Image src='/qrcode.png' alt='qr' width={400} height={400} />
-          </div>
-        </div>
-      )}
       <div className='container flex min-h-svh flex-col items-center justify-center space-y-6'>
         <Image
           src='/avatar.png'
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,13 @@
 
 import React from 'react';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { sourceCodePro } from '@/utils/font';
-import { Menu, Terminal } from 'lucide-react';
+import { Menu, QrCode, Terminal } from 'lucide-react';
 import { SiGithub } from 'react-icons/si';
 import ToggleTheme from '@/components/toggletheme';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 
 const Links: {
   title: string;
@@ -16,10 +17,11 @@ const Links: {
 
 const Header: React.FC = () => {
   const [headerHidden, setHeaderHidden] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [menuOpen, setMenuOpen] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 20) {
+      if (window.scrollY > 100) {
         setHeaderHidden(true);
       } else {
         setHeaderHidden(false);
@@ -35,11 +37,34 @@ const Header: React.FC = () => {
     setMenuOpen(false);
   };
 
+  const renderLinks = () => {
+    return Links.map((link) => (
+      <Link href={link.href} key={link.href}>
+        <Button variant={'link'} className='text-xl text-foreground'>
+          {link.title}
+        </Button>
+      </Link>
+    ));
+  };
+
   const transition = headerHidden && !menuOpen && 'transform -translate-y-full';
 
   return (
-    <header className='sticky top-0 z-50'>
-      <nav className='sticky -top-1 z-50 flex h-[74px] w-full flex-col items-center justify-center'>
+    <header className='sticky top-0 z-40'>
+      {isModalOpen && (
+        <div
+          className='container fixed inset-0 z-50 flex min-h-full min-w-full items-center justify-center bg-background/50'
+          onClick={() => setIsModalOpen(false)}
+        >
+          <div
+            className='select-none gap-3 rounded-lg bg-foreground/30 p-5'
+            onClick={(e) => e.stopPropagation()}
+          >
+            <Image src='/qrcode.png' alt='qr' width={400} height={400} />
+          </div>
+        </div>
+      )}
+      <nav className='flex h-[74px] w-full flex-col items-center justify-center'>
         {/* desktop */}
         <div
           className={`hidden w-full items-center justify-between space-x-4 border-b border-border bg-background/50 px-6 py-4 backdrop-blur-xl md:flex`}
@@ -53,19 +78,20 @@ const Header: React.FC = () => {
                 <span>caru.live</span>
               </div>
             </Link>
-            {Links.map((link) => (
-              <Link href={link.href} key={link.href}>
-                <Button variant={'link'} className='text-xl text-foreground'>
-                  {link.title}
-                </Button>
-              </Link>
-            ))}
+            {renderLinks()}
           </div>
           <div className='flex items-center space-x-4'>
             <ToggleTheme
               variant={'ghost'}
               className='border border-border dark:border-gray-700'
             />
+            <Button
+              variant={'outline'}
+              className='flex items-center space-x-2'
+              onClick={() => setIsModalOpen((prev) => !prev)}
+            >
+              <QrCode size={24} />
+            </Button>
             <Link href='https://github.com/caru-ini'>
               <SiGithub size={24} />
             </Link>
@@ -92,31 +118,22 @@ const Header: React.FC = () => {
           </Button>
         </div>
         {menuOpen && (
-          <div className='fixed left-0 top-[73px] z-50 flex size-full flex-col bg-background p-5 md:hidden'>
+          <div className='fixed left-0 top-[73px] z-40 flex size-full flex-col bg-background p-5 md:hidden'>
             <div className='flex flex-col space-y-4 text-2xl text-muted-foreground'>
-              <Link
-                href='/'
-                onClick={closeMenu}
-                className='hover:text-foreground'
-              >
-                Home
-              </Link>
-              {Links.map((link) => (
-                <Link
-                  href={link.href}
-                  key={link.href}
-                  onClick={closeMenu}
-                  className='hover:text-foreground'
-                >
-                  {link.title}
-                </Link>
-              ))}
+              {renderLinks()}
             </div>
             <div className='flex items-center justify-center space-x-4'>
               <ToggleTheme
                 variant={'ghost'}
                 className='mt-5 grow border border-border dark:border-gray-700'
               />
+              <Button
+                variant={'outline'}
+                className='mt-5'
+                onClick={() => setIsModalOpen((prev) => !prev)}
+              >
+                <QrCode size={24} />
+              </Button>
               <Link href='https://github.com/caru-ini'>
                 <SiGithub size={24} className='mt-5' />
               </Link>

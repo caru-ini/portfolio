d7cf0117b0aa0e8039fafda69e95992d7e26807d
caru-ini <caru17122@gmail.com>
2024-06-26 23:28:34 +0900

    fix: container height in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,44 +14,42 @@ import { buttonVariants } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className='bg-gradient-to-b from-background to-teal-200/10'>
-      <div className='container flex min-h-svh flex-col items-center justify-center space-y-6'>
-        <Image
-          src='/avatar.png'
-          alt='caru'
-          width={200}
-          height={200}
-          className='select-none rounded-full'
-          draggable={false}
-        />
-        <h1 className='text-4xl font-bold'>Caru</h1>
-        <Link
-          href={'/about'}
-          className={`flex items-center justify-center ${buttonVariants({ variant: 'secondary' })}`}
+    <main className='container flex min-h-[calc(100svh-74px)] flex-col items-center justify-center space-y-6 bg-gradient-to-b from-background to-teal-200/10'>
+      <Image
+        src='/avatar.png'
+        alt='caru'
+        width={200}
+        height={200}
+        className='select-none rounded-full'
+        draggable={false}
+      />
+      <h1 className='text-4xl font-bold'>Caru</h1>
+      <Link
+        href={'/about'}
+        className={`flex items-center justify-center ${buttonVariants({ variant: 'secondary' })}`}
+      >
+        Check My Profile
+        <MdChevronRight className='ml-2' size={24} />
+      </Link>
+      <div className='flex items-center space-x-4'>
+        <a href='https://twitter.com/caru_ini' target='_blank'>
+          <SiX size={32} />
+        </a>
+        <a href='https://github.com/caru-ini'>
+          <SiGithub size={32} />
+        </a>
+        <a href='https://zenn.dev/caru'>
+          <SiZenn size={32} />
+        </a>
+        <a
+          href='https://discordapp.com/users/1226826654794649690'
+          target='_blank'
         >
-          Check My Profile
-          <MdChevronRight className='ml-2' size={24} />
-        </Link>
-        <div className='flex items-center space-x-4'>
-          <a href='https://twitter.com/caru_ini' target='_blank'>
-            <SiX size={32} />
-          </a>
-          <a href='https://github.com/caru-ini'>
-            <SiGithub size={32} />
-          </a>
-          <a href='https://zenn.dev/caru'>
-            <SiZenn size={32} />
-          </a>
-          <a
-            href='https://discordapp.com/users/1226826654794649690'
-            target='_blank'
-          >
-            <SiDiscord size={32} />
-          </a>
-          <a href='https://speakerdeck.com/caru'>
-            <SiSpeakerdeck size={32} />
-          </a>
-        </div>
+          <SiDiscord size={32} />
+        </a>
+        <a href='https://speakerdeck.com/caru'>
+          <SiSpeakerdeck size={32} />
+        </a>
       </div>
     </main>
   );

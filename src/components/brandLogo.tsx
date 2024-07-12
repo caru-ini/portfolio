3725d76729d9cb25f3466356a62dc337b59e62f5
caru-ini <caru17122@gmail.com>
2024-07-12 17:27:05 +0900

    feat: add select-none and set undraggable

--- src/components/brandLogo.tsx
+++ src/components/brandLogo.tsx
@@ -3,7 +3,7 @@ import { Terminal } from 'lucide-react';
 
 export const BrandLogo: React.FC = () => (
   <div
-    className={`flex items-center gap-x-2 rounded-md border-2 border-blue-400 px-2 dark:border-teal-200 ${sourceCodePro.className} bg-slate-500 text-xl text-white`}
+    className={`flex items-center gap-x-2 rounded-md border-2 border-blue-400 px-2 dark:border-teal-200 ${sourceCodePro.className} select-none bg-slate-500 text-xl text-white`}
   >
     <Terminal />
     <span>caru.live</span>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -64,7 +64,7 @@ const Header: React.FC = () => {
           className={`hidden w-full items-center justify-between space-x-4 border-b border-border px-6 py-4 backdrop-blur-md md:flex ${scrollY <= 0 ? 'border-none bg-none' : 'bg-background/50'}`}
         >
           <div className='flex items-center space-x-2'>
-            <Link href='/'>
+            <Link href='/' draggable={false}>
               <BrandLogo />
             </Link>
             {renderLinks()}

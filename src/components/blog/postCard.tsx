41d40712fc04e8071fa09110f21e6f545afab227
caru-ini <caru17122@gmail.com>
2024-08-24 15:20:43 +0000

    feat: update props

--- src/components/blog/postCard.tsx
+++ src/components/blog/postCard.tsx
@@ -4,30 +4,34 @@ import { TagBadge } from './tagBadge';
 
 type BlogPostCardProps = {
   title: string;
-  date: string;
+  slug: string;
   excerpt: string;
-  href: string;
-  tags: string[];
+  tags?: string[];
+  updatedAt: string;
+  createdAt: string;
 };
 
-export const BlogPostCard: React.FC<BlogPostCardProps> = ({ title, date, excerpt, href, tags }) => {
+export const BlogPostCard: React.FC<BlogPostCardProps> = ({
+  title,
+  slug,
+  excerpt,
+  tags,
+  createdAt,
+}) => {
   return (
-    <div className='rounded-lg border p-4 transition-shadow hover:shadow-md'>
-      <h3 className='mb-2 text-xl font-semibold'>
-        <Link href={href} className='hover:text-primary'>
-          {title}
-        </Link>
-      </h3>
+    <Link
+      className='rounded-lg border p-4 transition-shadow hover:shadow-md'
+      href={'/blog/' + slug}
+    >
+      <h3 className='mb-2 text-xl font-semibold'>{title}</h3>
       <p className='mb-2 inline-flex items-center text-sm text-muted-foreground'>
         <CalendarDays size={16} className='mr-1' />
-        {date}
+        {new Date(createdAt).toLocaleDateString()}
       </p>
       <p className='text-muted-foreground'>{excerpt}</p>
       <div className='mt-2 flex flex-wrap'>
-        {tags.map((tag) => (
-          <TagBadge tag={tag} key={tag} />
-        ))}
+        {tags?.map((tag) => <TagBadge tag={tag} key={tag} />)}
       </div>
-    </div>
+    </Link>
   );
 };

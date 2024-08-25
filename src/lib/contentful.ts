8e6f1b71b651126c59470bd8e0f57c584e768b03
caru-ini <caru17122@gmail.com>
2024-08-25 15:20:55 +0000

    feat: update fetch method

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -4,3 +4,24 @@ export const contentful = createClient({
   space: process.env.CONTENTFUL_SPACE_ID!,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
 });
+
+export const getLatestPostIndex = async (limit = 10) => {
+  const entries = await contentful.getEntries({
+    content_type: 'post',
+    order: ['-sys.createdAt'],
+    limit: limit,
+  });
+
+  const mapped = entries.items.map((entry) => {
+    return {
+      id: entry.sys.id,
+      title: entry.fields.title,
+      slug: entry.fields.slug,
+      excerpt: entry.fields.excerpt,
+      tags: entry.metadata.tags.map((tag: any) => tag.sys.id),
+      createdAt: entry.sys.createdAt,
+    };
+  });
+
+  return mapped;
+};

bdab9cb071e50a92b5bd9443c3df15466f2e98ec
caru-ini <caru17122@gmail.com>
2024-08-26 04:12:40 +0000

    feat: add opengraph image

---
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -0,0 +1,34 @@
+import { getPostBySlug } from '@/lib/contentful';
+import { ImageResponse } from 'next/og';
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = 'image/png';
+
+type OpenGraphImageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+const Image = async ({ params: { slug } }: OpenGraphImageProps) => {
+  const post = await getPostBySlug(slug);
+  if (!post) {
+    return null;
+  }
+  return new ImageResponse(
+    (
+      <div className='p-8'>
+        <h1 className='text-3xl font-semibold'>{post.title}</h1>
+        <p className='text-lg text-muted-foreground'>
+          {new Date(post.createdAt).toLocaleDateString()}
+        </p>
+      </div>
+    ),
+  );
+};
+
+export default Image;

e88624e5a858c007d396fcf7bd95a98153bfb101
caru-ini <caru17122@gmail.com>
2024-08-29 06:46:32 +0000

    feat: update markdown config

--- src/lib/markdownIt.ts
+++ src/lib/markdownIt.ts
@@ -3,6 +3,7 @@ import markdownIt from 'markdown-it';
 export const mdIt = markdownIt({
   html: true,
   linkify: true,
+  typographer: true,
 });
 
 mdIt.linkify.set({ fuzzyEmail: false, fuzzyIP: false });

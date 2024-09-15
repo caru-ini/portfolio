758cd3d48ba7c2fbfc0ca159dcd37a7fd1f30cff
caru-ini <caru17122@gmail.com>
2024-09-15 12:56:55 +0000

    feat: add light theme styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,37 +15,39 @@ export default async function Home() {
   return (
     <main>
       {/* Hero */}
-      <section className='flex h-svh w-full flex-col items-center justify-center gap-y-6 bg-gray-100 bg-gradient-to-b dark:from-cyan-950 dark:to-background'>
-        <Avatar />
-        <div className='flex flex-col items-center'>
-          <h1 className='text-4xl font-bold'>Caru</h1>
-          <p className='text-lg text-muted-foreground'>Student / Developer</p>
-        </div>
-        <div className='flex items-center space-x-4'>
-          <a href='https://twitter.com/caru_ini' target='_blank' rel='noopener noreferrer'>
-            <SiX size={32} />
-          </a>
-          <a href='https://github.com/caru-ini' target='_blank' rel='noopener noreferrer'>
-            <SiGithub size={32} />
-          </a>
-          <a href='https://zenn.dev/caru' target='_blank' rel='noopener noreferrer'>
-            <SiZenn size={32} />
-          </a>
-          <a
-            href='https://discordapp.com/users/1226826654794649690'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <SiDiscord size={32} />
-          </a>
-          <a href='https://speakerdeck.com/caru' target='_blank' rel='noopener noreferrer'>
-            <SiSpeakerdeck size={32} />
-          </a>
-        </div>
-        {/* scroll */}
-        <div className='absolute bottom-4 flex flex-col items-center text-muted-foreground'>
-          <BsChevronDoubleDown className='animate-bounce' size={32} />
-          <span className='text-sm'>Learn more</span>
+      <section className='flex h-svh w-full flex-col bg-secondary'>
+        <div className='flex size-full flex-col items-center justify-center gap-y-6 bg-gradient-to-b from-cyan-100 to-background dark:from-cyan-950 dark:to-background'>
+          <Avatar />
+          <div className='flex flex-col items-center'>
+            <h1 className='text-4xl font-bold'>Caru</h1>
+            <p className='text-lg text-muted-foreground'>Student / Developer</p>
+          </div>
+          <div className='flex items-center space-x-4'>
+            <a href='https://twitter.com/caru_ini' target='_blank' rel='noopener noreferrer'>
+              <SiX size={32} />
+            </a>
+            <a href='https://github.com/caru-ini' target='_blank' rel='noopener noreferrer'>
+              <SiGithub size={32} />
+            </a>
+            <a href='https://zenn.dev/caru' target='_blank' rel='noopener noreferrer'>
+              <SiZenn size={32} />
+            </a>
+            <a
+              href='https://discordapp.com/users/1226826654794649690'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <SiDiscord size={32} />
+            </a>
+            <a href='https://speakerdeck.com/caru' target='_blank' rel='noopener noreferrer'>
+              <SiSpeakerdeck size={32} />
+            </a>
+          </div>
+          {/* scroll */}
+          <div className='absolute bottom-4 flex flex-col items-center text-muted-foreground'>
+            <BsChevronDoubleDown className='animate-bounce' size={32} />
+            <span className='text-sm'>Learn more</span>
+          </div>
         </div>
       </section>
       <section className='py-12 md:py-24 lg:py-32'>

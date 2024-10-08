2f7b11336003a0b2626efb2d2457a375a5367e8c
caru-ini <caru17122@gmail.com>
2024-10-08 13:07:47 +0000

    feat: use sorceCodePro instead of mono

--- src/components/sections/Creations.tsx
+++ src/components/sections/Creations.tsx
@@ -1,6 +1,66 @@
-import { CreationCard } from '@/components/creationCard';
+import { cn } from '@/lib/utils';
+import { sourceCodePro } from '@/utils/font';
 import { ExternalLink } from 'lucide-react';
 import { SiGithub } from 'react-icons/si';
+
+type Creation = {
+  title: string;
+  links: { href: string; icon: React.ElementType }[];
+  description: string;
+};
+
+const creations: Creation[] = [
+  {
+    title: 'My-Components',
+    links: [
+      { href: 'https://github.com/caru-ini/my-components', icon: SiGithub },
+      { href: 'https://components.caru.live/', icon: ExternalLink },
+    ],
+    description:
+      'shadcn/uiを使った自作のコンポーネント集。デモサイトもあります。しかし、肝心の載せるコンポーネントのアイデアがあまりないので、まだコンポーネントの数は少なめです。',
+  },
+  {
+    title: 'Hono + D1 Todo',
+    links: [{ href: 'https://github.com/caru-ini/hono-d1-todo', icon: SiGithub }],
+    description:
+      'HonoとCloudFlare D1、Next.jsを使ったTodoアプリ。D1にデータを保存しています。Vitestのなんちゃってテストもあります。 ご参考までにどうぞ。デプロイはしていません。',
+  },
+  {
+    title: 'wol-webui',
+    links: [{ href: 'https://github.com/caru-ini/wol-webui', icon: SiGithub }],
+    description:
+      '自分用に作ったWoL (Wake on LAN)のWebUI。Next.js + Hono + Prisma(PostgreSQL)で作成しています。docker-composeで使用できます。',
+  },
+  {
+    title: 'chat-cli',
+    links: [{ href: 'https://github.com/caru-ini/chat-cli', icon: SiGithub }],
+    description:
+      'Pythonで作成したリッチなChatGPT(API)のCLI。 openai + rich + prompt_toolkitなどを使っています。レスポンスのストリーミング表示、ツール、マルチセッション対応です。',
+  },
+];
+
+const CreationCard: React.FC<Creation> = ({ title, links, description }) => {
+  return (
+    <div className='flex flex-col gap-4'>
+      <h3
+        className={cn(
+          'w-fit rounded-lg bg-secondary/60 py-0.5 px-2 text-2xl font-semibold',
+          sourceCodePro.className,
+        )}
+      >
+        {title}
+      </h3>
+      <p className='text-muted-foreground'>{description}</p>
+      <div className='flex gap-2'>
+        {links.map((link, index) => (
+          <a key={index} href={link.href} target='_blank' rel='noopener noreferrer'>
+            <link.icon className='size-8' />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export function Creations() {
   return (
@@ -12,42 +72,9 @@ export function Creations() {
         <h2 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl'>Creations</h2>
         <p className='text-muted-foreground'>これまでに作成したアプリとかサイトとか</p>
         <div className='grid gap-8 p-2 md:grid-cols-2 '>
-          <CreationCard
-            title='My-Components'
-            links={[
-              { href: 'https://github.com/caru-ini/my-components', icon: <SiGithub size={32} /> },
-              { href: 'https://components.caru.live/', icon: <ExternalLink size={32} /> },
-            ]}
-          >
-            <code>shadcn/ui</code>を使った自作のコンポーネント集。デモサイトもあります。
-            しかし、肝心の載せるコンポーネントのアイデアがあまりないので、まだコンポーネントの数は少なめです。
-          </CreationCard>
-          <CreationCard
-            title='Hono + D1 Todo'
-            links={[
-              { href: 'https://github.com/caru-ini/hono-d1-todo', icon: <SiGithub size={32} /> },
-            ]}
-          >
-            HonoとCloudFlare D1、Next.jsを使ったTodoアプリ。D1にデータを保存しています。
-            Vitestのなんちゃってテストもあります。 ご参考までにどうぞ。デプロイはしていません。
-          </CreationCard>
-          <CreationCard
-            title='wol-webui'
-            links={[
-              { href: 'https://github.com/caru-ini/wol-webui', icon: <SiGithub size={32} /> },
-            ]}
-          >
-            自分用に作ったWoL (Wake on LAN)のWebUI。Next.js + Hono +
-            Prisma(PostgreSQL)で作成しています。docker-composeで使用できます。
-          </CreationCard>
-          <CreationCard
-            title='chat-cli'
-            links={[{ href: 'https://github.com/caru-ini/chat-cli', icon: <SiGithub size={32} /> }]}
-          >
-            Pythonで作成したリッチなChatGPT(API)のCLI。 openai + rich +
-            prompt_toolkitなどを使っています。
-            レスポンスのストリーミング表示、ツール、マルチセッション対応です。
-          </CreationCard>
+          {creations.map((creation) => (
+            <CreationCard key={creation.title} {...creation} />
+          ))}
         </div>
       </div>
     </section>

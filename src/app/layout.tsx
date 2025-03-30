f1612da2935106fd071f4f56662b614ab3677fea
caru-ini <caru17122@gmail.com>
2025-03-30 13:44:07 +0000

    feat: add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import { env } from "@/env";
 import { inter, poppins } from "@/lib/fonts";
+import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import "./globals.css";
@@ -19,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" suppressHydrationWarning className={`${poppins.variable} ${inter.variable}`}>
+      <GoogleTagManager gtmId={env.NEXT_PUBLIC_GTM_ID ?? ""} />
       <SessionProvider>
         <body className="flex min-h-svh flex-col" suppressHydrationWarning>
           <ThemeProvider>{children}</ThemeProvider>
--- src/env/client.js
+++ src/env/client.js
@@ -1,4 +1,5 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   /**
@@ -8,12 +9,14 @@ export const env = createEnv({
   client: {
     // Empty object as there are no client environment variables yet
     // Example: NEXT_PUBLIC_API_URL: z.string().url(),
+    NEXT_PUBLIC_GTM_ID: z.string().optional(),
   },
   /**
    * Actual values for client environment variables
    */
   runtimeEnv: {
     // Example: NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_GTM_ID: process.env.NEXT_PUBLIC_GTM_ID,
   },
   /**
    * Treat empty strings as undefined

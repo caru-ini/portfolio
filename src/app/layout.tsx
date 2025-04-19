ced0d7eae33895adae688eebe7a1cb115ee6049d
caru-ini <caru17122@gmail.com>
2025-04-19 13:49:09 +0000

    feat: add mPLUS 1p for main japanese font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { env } from "@/env";
-import { inter, poppins } from "@/lib/fonts";
+import { inter, mPlus1p, poppins } from "@/lib/fonts";
 import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
@@ -20,7 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" suppressHydrationWarning className={`${poppins.variable} ${inter.variable}`}>
+    <html
+      lang="ja"
+      suppressHydrationWarning
+      className={`${mPlus1p.variable} ${poppins.variable} ${inter.variable}`}
+    >
       <GoogleTagManager gtmId={env.NEXT_PUBLIC_GTM_ID ?? ""} />
       <SessionProvider>
         <body className="flex min-h-svh flex-col" suppressHydrationWarning>
--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,8 +1,8 @@
-import { Inter, Poppins } from "next/font/google";
+import { Inter, M_PLUS_1p, Poppins } from "next/font/google";
 
 export const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["700"],
   variable: "--font-poppins",
   display: "swap",
 });
@@ -10,5 +10,12 @@ export const poppins = Poppins({
 export const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
+  display: "swap",
+});
+
+export const mPlus1p = M_PLUS_1p({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-mPlus1p",
   display: "swap",
 });

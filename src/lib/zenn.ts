c45b982993455308913dfb9ca45f41691b12cffb
caru-ini <caru17122@gmail.com>
2025-09-27 22:30:28 +0900

    feat: disable text generation in development env

--- src/lib/zenn.ts
+++ src/lib/zenn.ts
@@ -13,16 +13,18 @@ export interface ZennArticleItem {
 const parser = new Parser<object, ZennArticleItem>();
 
 const generateWithGeminiCached = cache(async (input: string) => {
-  return await generateWithGemini({
-    instructions: `## Instructions
+  return process.env.NODE_ENV === "production"
+    ? await generateWithGemini({
+        instructions: `## Instructions
     以下の記事の技術的な詳細や重要なポイントについてまとめた簡潔な説明を書いてください。
     筆者について書く必要はありません。
     出力はプレーンテキストで、日本語(敬体)で記述してください。
     この要約は筆者"かる(@caru_ini)"のサイトのブログ記事紹介セクションで、ブログ記事カードの短い説明として使用されます。
     読者の興味を引く(ただしフォーマルに)、わかりやすい説明を心がけてください。
     出力は100文字以内に収めてください。`,
-    input,
-  });
+        input,
+      })
+    : "Disabled in development environment";
 });
 
 const getZennFeed = async (feedUrl: string) => {

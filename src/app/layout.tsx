e71d4933e17c517511e7d2bdd755ce6696ef982f
caru-ini <caru17122@gmail.com>
2025-12-24 00:13:21 +0900

    refactor: remove auth for simplicity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { env } from "@/env";
 import { inter, mPlus1p, poppins } from "@/lib/fonts";
 import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
-import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -37,14 +36,12 @@ export default function RootLayout({
       className={`${mPlus1p.variable} ${poppins.variable} ${inter.variable}`}
     >
       <GoogleTagManager gtmId={env.NEXT_PUBLIC_GTM_ID ?? ""} />
-      <SessionProvider>
-        <body className="flex min-h-svh flex-col" suppressHydrationWarning>
-          <ThemeProvider>
-            {children}
-            <ScrollToTopButton />
-          </ThemeProvider>
-        </body>
-      </SessionProvider>
+      <body className="flex min-h-svh flex-col" suppressHydrationWarning>
+        <ThemeProvider>
+          {children}
+          <ScrollToTopButton />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }

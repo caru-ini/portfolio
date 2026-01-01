26c3539a80c25c8fa71612696f483608b5c7a06a
caru-ini <caru17122@gmail.com>
2026-01-01 22:50:40 +0900

    feat: use .woff2 for better performance

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -5,7 +5,7 @@ import LocalFont from "next/font/local";
 export const poppins = LocalFont({
   src: [
     {
-      path: "./font-files/Poppins-Bold.ttf",
+      path: "./font-files/poppins-v24-latin-700.woff2",
       weight: "700",
       style: "normal",
     },
@@ -23,12 +23,12 @@ export const inter = Inter({
 export const mPlus1p = LocalFont({
   src: [
     {
-      path: "./font-files/MPLUS1p-Regular.ttf",
+      path: "./font-files/m-plus-1p-v32-japanese_latin-regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./font-files/MPLUS1p-Bold.ttf",
+      path: "./font-files/m-plus-1p-v32-japanese_latin-700.woff2",
       weight: "700",
       style: "normal",
     },

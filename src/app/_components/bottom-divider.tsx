bab17f829f280ecc17dc9ca0ab3a57d137171750
caru-ini <caru17122@gmail.com>
2026-02-25 00:43:21 +0900

    feat: add bottom divider

---
+++ src/app/_components/bottom-divider.tsx
@@ -0,0 +1,9 @@
+export function BottomDivider() {
+  return (
+    <div className="w-full flex h-2">
+      <div className="flex-1 bg-foreground" />
+      <div className="bg-primary w-16" />
+      <div className="bg-pink-300 w-10" />
+    </div>
+  );
+}
